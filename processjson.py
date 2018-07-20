d78d9c44bd89dcfda5420e14d5b9e4a6d78d7ce1
rahul1205 <anand.rahul1205@gmail.com>
2018-07-20 18:15:22 +0530

    initial commit

--- processjson.py
+++ processjson.py
@@ -1,6 +1,23 @@
 import json
+import re
+from nltk import NaiveBayesClassifier
 
-with open('dataset.json') as json_data:
-    data = json.load(json_data)
-    for d in data:
-        print d.get('tweet')
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub(
+        "(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+
+mydata = []
+json_data = open('convertcsv.json', 'r')
+data = json.load(json_data)
+for d in data:
+    if d.get('hate_speech') == 0:
+        mydata.append(
+            {"text": clean_tweet(d.get('tweet')), "label": "pos"})
+    else:
+        mydata.append(
+            {"text": clean_tweet(d.get('tweet')), "label": "neg"})
+
+cl = NaiveBayesClassifier(mydata, format="json")
+cl.classify("This is an amazing library!")
